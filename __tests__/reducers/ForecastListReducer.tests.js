a5853b9ea3e6890775f3d0d163701a7976a2cdb1
Michael Brown and Sheila Stephen <sheila@>
2019-03-06 15:14:35 -0800

    remove depricated tests

--- __tests__/reducers/ForecastListReducer.tests.js
+++ __tests__/reducers/ForecastListReducer.tests.js
@@ -1,11 +1,9 @@
-import constants from './../../src/constants';
+
 import ForecastListReducer from './../../src/reducers/ForecastListReducer';
-import * as actions from './../../src/actions';
-import { createStore } from 'redux';
+
 
 describe('ForecastApp', () => {
-  // const { initialState, types } = constants;
-  const store = createStore(ForecastListReducer, initialState);
+
 
   describe('ForecastListReducer', () => {
     it('return input state if we give it type null', () => {
