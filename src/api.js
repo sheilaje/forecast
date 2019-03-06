0a73607eeefce7706e0c6cba9847739cfab5bb82
Michael Brown and Sheila Stephen <sheila@>
2019-03-06 11:28:48 -0800

    get weather details on the console

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 function forecastCall(place='portland') {
-  return fetch(`http://api.apixu.com/v1/current.json?key=0ad69ecb045c4297a0e52409190603&q=${place}`).then(
+  return fetch(`http://api.apixu.com/v1/forecast.json?key=0ad69ecb045c4297a0e52409190603&q=${place}&days=7`).then(
     response => response.json(),
-    error => console.log('An error occured. ', error);
+    error => console.log('An error occured. ', error)
   ).then((json)=> {
     return json
   });
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { createForecastAction } from '../actions';
+import ForecastListReducer from '../reducers';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.getForecast = this.getForecast.bind(this);
+  }
+
+  getForecast(){
+    const { dispatch } = this.props;
+    let forecastPromise = createForecastAction();
+    forecastPromise.then((getForecastAction)=>{
+      return dispatch(getForecastAction);
+    })
+  }
+
   render() {
-    return (
-      <div>
-        App works
-      </div>
-    );
+    if(!this.props.forecastWeek){
+      return (
+        <div>
+          <button onClick={this.getForecast}>get weather</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Got it
+        </div>
+      )
+    }
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    forecastWeek: state.forecastWeek
+  }
+}
+
+export default connect(mapStateToProps)(App);
