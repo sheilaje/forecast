609138324756ac4ea0078795da608aeee8645451
Michael Brown and Sheila Stephen <michael@>
2019-03-06 12:17:50 -0800

    fix propTypes for weather components

--- src/components/WeatherList.jsx
+++ src/components/WeatherList.jsx
@@ -18,16 +18,13 @@ WeatherList.propTypes = {
     PropTypes.shape({
       date: PropTypes.string,
       date_epoch: PropTypes.number,
-      day: PropTypes.objectOf(
-        PropTypes.oneOfType[
-          PropTypes.number,
-          PropTypes.shape({
-            text: PropTypes.string.isRequired,
-            icon: PropTypes.string,
-            code: PropTypes.number
-          })
-        ]
-      ),
+      day: PropTypes.shape({
+        condition: PropTypes.shape({
+          text: PropTypes.string.isRequired,
+          icon: PropTypes.string,
+          code: PropTypes.number
+        }).isRequired
+      }),
       astro: PropTypes.objectOf(PropTypes.string)
     })
   ).isRequired
